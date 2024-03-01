c30a7b754b10043292ab00e48be4dc89e816db88
barkinspmz <barkinsapmaz2@outlook.com>
2024-03-01 14:47:19 +0300

    Scriptable object has initiated. It will store all the question datas.

---
+++ QuizMasterPractice/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+
+public class QuestionSO : ScriptableObject
+{
+    [SerializeField] string question = "Enter the question here";
+}
